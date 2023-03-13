ecf9e1528251b62e1c02a4c276b16fd4e3b7a304
Thein Naing <theinnaing0908@gmail.com>
2023-03-13 17:23:51 +0800

    Adds missing null constraints to flats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_091449) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_092147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_091449) do
   end
 
   create_table "flats", force: :cascade do |t|
-    t.text "description"
-    t.integer "price_per_night"
+    t.text "description", null: false
+    t.integer "price_per_night", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "owner_id"
