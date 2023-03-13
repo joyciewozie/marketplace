97f9802208730aa673a82ae645f45838265572d0
Thein Naing <theinnaing0908@gmail.com>
2023-03-13 17:19:33 +0800

    Adds associations to user, flat and booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,4 @@
 class Booking < ApplicationRecord
+  belongs_to :customer, class: 'User', foreign_key: :customer_id
+  belongs_to :flat
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,7 +1,4 @@
 class Flat < ApplicationRecord
-    # belongs_to :users
-    # has_many :bookings
-
-    validates :description, presence: true
-    validates :price_per_night, presence: true
+  belongs_to :owner, class: 'User', foreign_key: :owner_id
+  has_many :bookings
 end
