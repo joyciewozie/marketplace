5edf3fa3fdded342a4ddd1bc9e10aaa1ba3f9f18
Thein Naing <theinnaing0908@gmail.com>
2023-03-13 18:06:07 +0800

    Adds new name and location columns to flats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_092147) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_095131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_092147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "owner_id"
+    t.string "name", null: false
+    t.string "location", null: false
     t.index ["owner_id"], name: "index_flats_on_owner_id"
   end
 
