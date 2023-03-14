52c60c609b132fa4fe675f72602e7bcc51d6b323
Thein Naing <theinnaing0908@gmail.com>
2023-03-14 14:16:28 +0800

    flat controller and new view are implemented

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,21 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    Flat.create
+    redirect_to flat_path([:id])
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:description, :price_per_night, :owner_id, :name, :location)
+  end
 end
