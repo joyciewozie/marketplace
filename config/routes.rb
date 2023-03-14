06e21380900ed04b8c7c0ec086b8231b1d3d3ab1
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-14 13:13:54 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users do
-    resources :flats
+  resources :users
+
+  resources :flats do
     resources :bookings
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
