ea6c1b74b57d952087acdfc452c93aa8c96d534b
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-14 13:24:58 +0800

    Revert "Update routes & added template"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users
-
-  resources :flats do
+  resources :users do
+    resources :flats
     resources :bookings
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
