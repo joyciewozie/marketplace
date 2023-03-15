47e3143ecf6aaec85a2b0014c178ccae0822a07a
Thein Naing <theinnaing0908@gmail.com>
2023-03-15 11:47:41 +0800

    Modify the model, controller and views for flat

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,10 +1,10 @@
 class Flat < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   has_many :bookings
+  has_one_attached :photo
 
   validates :description, presence: true
   validates :price_per_night, presence: true
   validates :name, presence: true
   validates :location, presence: true
-  has_one_attached :photo
 end
