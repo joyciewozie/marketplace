3d9e949c41889b71a7ea8d16b538cad982b8557e
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-16 23:54:36 +0800

    created a new route for all bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :flats do
     resources :bookings, only: [:create]
   end
+  resources :bookings, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
