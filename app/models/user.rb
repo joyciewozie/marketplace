2970c22a4f6a4ac5075de8d88b67dd27d243396b
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-16 13:26:53 +0800

    added customer_id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :flats
-  has_many :bookings
+  has_many :bookings, foreign_key: :customer_id
 
   validates :name, presence: true
 end
